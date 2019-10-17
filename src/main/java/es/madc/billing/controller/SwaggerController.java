da2a1034cf0790b7deb4cbeba119720c89a0fd85
Miguel Angel Diaz Calvo <03912502H@renfeviajeros365.es>
2019-10-17 09:13:51 +0200

    No carga swagger-ui

--- src/main/java/es/madc/billing/controller/SwaggerController.java
+++ src/main/java/es/madc/billing/controller/SwaggerController.java
@@ -1,13 +1,13 @@
 package es.madc.billing.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class SwaggerController {
 
-	@GetMapping("/")
+	@RequestMapping("/")
 	public ModelAndView swaggerUI() {
 		return new ModelAndView("redirect:swagger-ui.html");
 	}
